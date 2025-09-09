92167ce4f318b9be8b4fa7d99b260d63a80aa826
Valentino Picco <valentinopicco2004@gmail.com>
2025-09-09 15:18:34 -0300

    model teacher

---
+++ alumnos_girasoles/lib/models/teacher.dart
@@ -0,0 +1,37 @@
+import 'package:alumnos_girasoles/enums/enums.dart';
+
+class Teacher {
+  final int dni;
+  final String surname;
+  final String name;
+  final Grade? gradeHolder;
+  final String authId;
+
+  Teacher({
+    required this.dni,
+    required this.surname,
+    required this.name,
+    this.gradeHolder,
+    required this.authId,
+  });
+
+  factory Teacher.fromMap(Map<String, dynamic> map) {
+    return Teacher(
+      dni: map['dni'],
+      surname: map['surname'],
+      name: map['name'],
+      gradeHolder: map['grade_holder'],
+      authId: map['auth_id'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'dni': dni,
+      'surname': surname,
+      'name': name,
+      'grade_holder': gradeHolder,
+      'auth_id': authId,
+    };
+  }
+}
