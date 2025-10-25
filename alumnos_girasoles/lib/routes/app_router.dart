c897355aa6ed497f68733d25b17d1a545791d034
Valentino Picco <valentinopicco2004@gmail.com>
2025-10-25 15:38:21 -0300

    comienzo de rutas

--- alumnos_girasoles/lib/routes/app_router.dart
+++ alumnos_girasoles/lib/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:alumnos_girasoles/views/login.dart';
+import 'package:alumnos_girasoles/views/register.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -6,6 +7,8 @@ class AppRouter {
     switch (settings.name) {
       case LoginScreen.routeName:
         return MaterialPageRoute(builder: (_) => LoginScreen());
+      case RegisterScreen.routeName:
+        return MaterialPageRoute(builder: (_) => RegisterScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
